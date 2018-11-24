810d340ae5de65142f23302516c175018569f71e
EugeneDz <e.dzhevadov@gmail.com>
2018-11-24 22:26:23 +0200

    feat(app): add mongodb connection

---
+++ app/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://@ds115244.mlab.com:15244/online-cv',
+  mongoUser: 'eugene',
+  mongoPasswd: 'eugene123',
+};
--- app/index.js
+++ app/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB Config
+const db = require('./config/keys');
+
+// Connect to MongoDB
+mongoose
+  .connect(db.mongoURI, {
+    useNewUrlParser: true,
+    auth: {
+      user: db.mongoUser,
+      password: db.mongoPasswd,
+    },
+  })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(error => console.log(error));
 
 app.get('/', (req, res) => res.send('Hello'));
 

0d382e3df56242099ee271b624e43e08db9b0167
EugeneDz <e.dzhevadov@gmail.com>
2018-11-25 16:49:46 +0200

    feat(profile): add create/update profile route

--- app/routes/api/profile.js
+++ app/routes/api/profile.js
@@ -29,4 +29,60 @@ router.get('/', passport.authenticate('jwt', { session: false }), async (req, re
   }
 });
 
+// @route   POST api/profile
+// @desc    Create/Edit user profile
+// @access  Private
+router.post('/', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  try {
+    const errors = {};
+    const profileFields = {};
+    profileFields.user = req.user.id;
+
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername) profileFields.githubusername = req.body.githubusername;
+    if (req.body.stackoverflowusername) {
+      profileFields.stackoverflowusername = req.body.stackoverflowusername;
+    }
+
+    // Skills
+    if (typeof req.body.skills !== 'undefined') {
+      profileFields.skills = req.body.skills.split(',');
+    }
+
+    // Social
+    profileFields.social = {};
+    if (req.body.youtube) profileFields.skills.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.skills.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.skills.facebook = req.body.facebook;
+    if (req.body.linkedin) profileFields.skills.linkedin = req.body.linkedin;
+
+    const profile = await Profile.findOne({ user: req.user.id });
+    if (profile) {
+      const updatedProfile = await Profile.findOneAndUpdate(
+        { user: req.user.id },
+        { $set: profileFields },
+        { new: true },
+      );
+      return res.json(updatedProfile);
+    }
+
+    const hasHandle = await Profile.findOne({ handle: profileFields.handle });
+    if (hasHandle) {
+      errors.handle = 'That handle already exists';
+
+      return res.status(400).json(errors);
+    }
+
+    const newProfile = await new Profile(profileFields).save();
+    return res.json(newProfile);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
+
 module.exports = router;

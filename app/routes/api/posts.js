9ad118dc40f3a2673836a319548045a0f5bd968b
EugeneDz <e.dzhevadov@gmail.com>
2018-11-28 21:53:56 +0200

    feat(post): add like & unlike post route

--- app/routes/api/posts.js
+++ app/routes/api/posts.js
@@ -74,4 +74,60 @@ router.delete('/:post_id', passport.authenticate('jwt', { session: false }), asy
   return res.json({ success: true });
 });
 
+// @route   POST api/posts/like/:post_id
+// @desc    Like post
+// @access  Private
+router.post(
+  '/like/:post_id',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res) => {
+    const errors = {};
+
+    const post = await Post.findById(req.params.post_id);
+    if (post.user.toString() !== req.user.id) {
+      errors.profile = 'User not authorized';
+      return res.status(401).send(errors);
+    }
+
+    const hasLiked = post.likes.filter(like => like.user.toString() === req.user.id).length;
+    if (hasLiked) {
+      errors.likes = 'User already liked this post';
+      return res.status(400).send(errors);
+    }
+
+    post.likes.unshift({ user: req.user.id });
+    const savedPost = await post.save();
+    return res.json(savedPost);
+  },
+);
+
+// @route   POST api/posts/unlike/:post_id
+// @desc    Unlike  post
+// @access  Private
+router.post(
+  '/unlike/:post_id',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res) => {
+    const errors = {};
+
+    const post = await Post.findById(req.params.post_id);
+    // if (post.user.toString() !== req.user.id) {
+    //   errors.profile = 'User not authorized';
+    //   return res.status(401).send(errors);
+    // }
+
+    const hasLiked = post.likes.filter(like => like.user.toString() === req.user.id).length;
+    if (hasLiked === 0) {
+      errors.likes = 'Yo have not yet liked  this post';
+      return res.status(400).send(errors);
+    }
+
+    const index = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+    post.likes.splice(index, 1);
+
+    const savedPost = await post.save();
+    return res.json(savedPost);
+  },
+);
+
 module.exports = router;

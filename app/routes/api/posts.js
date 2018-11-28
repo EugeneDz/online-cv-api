518e05d7b6e08fde9a2dff09b9b75e182ee05150
EugeneDz <e.dzhevadov@gmail.com>
2018-11-28 21:28:58 +0200

    feat(post): add get & delete post route

--- app/routes/api/posts.js
+++ app/routes/api/posts.js
@@ -12,8 +12,32 @@ const router = express.Router();
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Posts works' }));
 
+// @route   GET api/posts
+// @desc    Get posts
+// @access  Public
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    return res.json(posts);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
+
+// @route   GET api/posts/:post_id
+// @desc    Get posts by id
+// @access  Public
+router.get('/:post_id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+    return res.json(post);
+  } catch (err) {
+    return res.status(404).send(err);
+  }
+});
+
 // @route   POST api/posts
-// @desc    create post
+// @desc    Create post
 // @access  Private
 router.post('/', passport.authenticate('jwt', { session: false }), async (req, res) => {
   try {
@@ -32,6 +56,22 @@ router.post('/', passport.authenticate('jwt', { session: false }), async (req, r
   } catch (err) {
     return res.status(500).send(err);
   }
+});
+
+// @route   DELETE api/posts/:post_id
+// @desc    Delete post by ID
+// @access  Private
+router.delete('/:post_id', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  const errors = {};
+  const post = await Post.findById(req.params.post_id);
+
+  if (post.user.toString() !== req.user.id) {
+    errors.profile = 'User not authorized';
+    return res.status(401).send(errors);
+  }
+
+  await post.delete();
+  return res.json({ success: true });
 });
 
 module.exports = router;
